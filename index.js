64de2fb172c4b647c96dd539a381a10056760027
Dan Woods <danwoodlondon@gmail.com>
2019-06-25 10:21:26 +0100

    Make exported function

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const potatoPie = require('potato-pie');
 
-const pie = potatoPie();
+const famousPieShop = () => {
+  const pie = `
+  Welcome to the pie shop! Here is your pie.
+  ------------------------------------------
 
-console.log('Welcome to the pie shop! Here is your pie.');
-console.log('------------------------------------------');
-console.log('');
-console.log(pie);
-console.log('');
-console.log('Thank you for your custom. Bye!');
+  ${potatoPie()}
+
+  Thank you for your custom. Bye!`;
+
+  console.log(pie);
+};
+
+module.exports = famousPieShop;
+
